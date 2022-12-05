1958033936ee4c6dff5f81227c7d618286851e82
Maxim <maxim.geer@yandex.ru>
2022-12-05 17:33:47 +0500

    logs for check personal data

--- src/pages/EmployeeList.vue
+++ src/pages/EmployeeList.vue
@@ -63,6 +63,11 @@ export default {
     },
   },
   mounted() { //TODO query
+    console.log('данные от телеграм WebAppInitData:');
+    console.log(window.Telegram.WebApp.WebAppInitData);
+
+    console.log('данные от телеграм WebAppUser:');
+    console.log(window.Telegram.WebApp.WebAppUser);
     axios
         .get("https://0b21-185-233-200-96.eu.ngrok.io/get_all_employees_on_id_deportament/?id_deportament=8",
           {
